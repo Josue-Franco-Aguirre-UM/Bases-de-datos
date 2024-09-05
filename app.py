28c94fd11dafa814693339ae8036d570778f51af
Josue Franco <jfranco100767@umanizales.edu.co>
2024-09-04 20:05:58 -0500

    commit 04092024

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-def _extract_students_from_excel(excel_file):
-        """Extracts student information from the provided Excel file."""
+def _extract_employees_from_excel(excel_file):
+        """Extracts employee information from the provided Excel file."""
         try:
             df = pd.read_excel(excel_file)
         except Exception as e:
@@ -10,26 +10,43 @@ def _extract_students_from_excel(excel_file):
             return []
 
         df = df.rename(columns={
-            'Código': 'code',
-            'Nombre': 'first_name',
-            'Apellidos': 'last_name',
+            'codigo_Empleado': 'code',
+            'Empleado': 'employee',
             'Email': 'email',
-            'Email Institucional': 'institutional_email'
         })
 
         df['code'] = df['code'].astype(str)
-        df['fullName'] = df['first_name'] + ' ' + df['last_name']
-        df['emails'] = df['email'] + ', ' + df['institutional_email']
         
-        df = df[['code', 'fullName', 'emails']]
+        st.write(df)
+
+def _extract_employees_details_from_excel(excel_file):
+        """Extracts employee information from the provided Excel file."""
+        try:
+            df = pd.read_excel(excel_file)
+        except Exception as e:
+            st.write(f"Error reading the Excel file: {e}")
+            return []
+
+        df = df.rename(columns={
+            'codigo_Empleado': 'code',
+            'Area': 'area',
+            'Horario': 'schedule',
+        })
+
+        df['code'] = df['code'].astype(str)
         
         st.write(df)
 
 
-st.title("Upload students attendance list Excel file")
+st.title("Upload employee information Excel files")
 
-uploaded_file = st.file_uploader("Attendance list Excel file", type=["xls", "xlsx"])
+uploaded_file_employees = st.file_uploader("Employee list Excel file", type=["xls", "xlsx"])
+uploaded_file_details = st.file_uploader("Employee details list Excel file", type=["xls", "xlsx"])
 
-if uploaded_file is not None:
-    st.write("file was uploaded successfully.")
-    _extract_students_from_excel(uploaded_file)
+if uploaded_file_employees is not None:
+    st.write("Employee list file was uploaded successfully.")
+    _extract_employees_from_excel(uploaded_file_employees)
+
+if uploaded_file_details is not None:
+    st.write("Employee details file was uploaded successfully.")
+    _extract_employees_details_from_excel(uploaded_file_details)
